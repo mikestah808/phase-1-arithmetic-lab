71a2db67a855a1902d80e609e7932613d2af30b8
Michael Stafford <michaelstafford808@gmail.com>
2021-11-23 12:17:37 -0800

    commit tree 70702b165c825f3e3716488b8a97a295aef91a4b

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+let multiply = num1 * num2;
+let num1 = 31;
+let num2 = 2;
+multiply = 62;
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 / num4;
+let num3 = 16;
+let num4 = 4;
+mod = 4;
+
+let max = Math.max(2,5,10,20);
+
